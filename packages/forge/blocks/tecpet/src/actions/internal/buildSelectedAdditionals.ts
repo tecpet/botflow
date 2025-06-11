25381a479c29dc8955569815725e3e7dc2382c95
Lucas Gomes <45773112+lucasgomesdef@users.noreply.github.com>
2025-06-11 11:20:17 -0300

    Selected additional array and restructure directories

---
+++ packages/forge/blocks/tecpet/src/actions/internal/buildSelectedAdditionals.ts
@@ -0,0 +1,42 @@
+import {createAction, option} from "@typebot.io/forge";
+import {baseOptions} from "../../constants";
+
+export const buildSelectedAdditionals = createAction({
+  baseOptions,
+  name: "Construir array de adicionais selecionados",
+  options: option.object({
+    selectedAdditional: option.string.layout({
+      label: "Adicional selecionado",
+      isRequired: true,
+      helperText: "Adicional selecionado",
+    }),
+    currentAdditionalArray: option.string.layout({
+      label: "Array de adicionais selecionados",
+      isRequired: true,
+      helperText: "Array de adicionais selecionados",
+    }),
+    updatedAdditionalArray: option.string.layout({
+      label: "Array de adicionais selecionados atualizado",
+      placeholder: "Selecione",
+      inputType: "variableDropdown",
+    }),
+  }),
+  getSetVariableIds: ({pets}) => (pets ? [pets] : []),
+  run: {
+    server: async ({options, variables, logs}) => {
+      try {
+        const rawInput = options.currentAdditionalArray ?? "[]";
+        const additionalArray: string[] = typeof rawInput === "string" ? JSON.parse(rawInput) : rawInput;
+        const withoutDuplicates = new Set(additionalArray);
+        withoutDuplicates.add(options.selectedAdditional);
+        const finalArray = [...withoutDuplicates];
+        variables.set([{id: options.updatedAdditionalArray, value: finalArray},]);
+      } catch (error) {
+        logs.add({
+          status: "error",
+          description: JSON.stringify(error),
+        });
+      }
+    },
+  },
+});
---
+++ packages/forge/blocks/tecpet/src/actions/internal/buildServiceSelection.ts
@@ -0,0 +1,103 @@
+import {createAction, option} from "@typebot.io/forge";
+import {baseOptions} from "../../constants";
+
+export const buildServiceSelectionSettings = createAction({
+  baseOptions,
+  name: "Construir configurações de seleção de serviço",
+  options: option.object({
+    selectedMenuConfigurations: option.string.layout({
+      label: "Configurações do menu selecionado",
+      isRequired: true,
+      helperText: "Configurações",
+    }),
+    serviceSelectionEnabled: option.string.layout({
+      label: "Serviços do agendamento habilitado",
+      placeholder: "Selecione",
+      inputType: "variableDropdown",
+    }),
+    serviceSelectionMessage: option.string.layout({
+      label: "Serviços do agendamento mensagem",
+      placeholder: "Selecione",
+      inputType: "variableDropdown",
+    }),
+    serviceSelectionValueMode: option.string.layout({
+      label: "Serviços do agendamento modo exibição de valor",
+      placeholder: "Selecione",
+      inputType: "variableDropdown",
+    }),
+    additionalSelectionEnabled: option.string.layout({
+      label: "Adicionais do agendamento habilitado",
+      placeholder: "Selecione",
+      inputType: "variableDropdown",
+    }),
+    additionalSelectionMessage: option.string.layout({
+      label: "Adicionais do agendamento mensagem",
+      placeholder: "Selecione",
+      inputType: "variableDropdown",
+    }),
+    professionalSelectionEnabled: option.string.layout({
+      label: "Profissional do agendamento habilitado",
+      placeholder: "Selecione",
+      inputType: "variableDropdown",
+    }),
+    professionalSelectionMessage: option.string.layout({
+      label: "Profissional do agendamento mensagem",
+      placeholder: "Selecione",
+      inputType: "variableDropdown",
+    }),
+    promotionSelectionEnabled: option.string.layout({
+      label: "Promoções do agendamento habilitado",
+      placeholder: "Selecione",
+      inputType: "variableDropdown",
+    }),
+  }),
+  run: {
+    server: async ({options, variables, logs}) => {
+      try {
+        const selectedMenuConfig =
+          typeof options.selectedMenuConfigurations === 'string'
+            ? JSON.parse(options.selectedMenuConfigurations)
+            : options.selectedMenuConfigurations as any;
+
+        const infoCollectionMenus = selectedMenuConfig.infoCollectionMenus;
+        const serviceSelection = infoCollectionMenus.serviceSelection;
+
+        console.log(serviceSelection);
+        variables.set([{id: options.serviceSelectionEnabled, value: Boolean(serviceSelection.service.enabled)}]);
+        variables.set([{id: options.serviceSelectionMessage, value: serviceSelection.service.message ?? ''}]);
+
+        variables.set([{
+          id: options.serviceSelectionValueMode,
+          value: serviceSelection.showServiceValues.priceDisplayMode
+        }]);
+
+        variables.set([{
+          id: options.additionalSelectionEnabled,
+          value: Boolean(serviceSelection.serviceAddons.enabled)
+        }]);
+        variables.set([{id: options.additionalSelectionMessage, value: serviceSelection.serviceAddons.message ?? ''}]);
+
+        variables.set([{
+          id: options.professionalSelectionEnabled,
+          value: Boolean(serviceSelection.serviceProfessionalChoice.enabled)
+        }]);
+        variables.set([{
+          id: options.professionalSelectionMessage,
+          value: serviceSelection.serviceProfessionalChoice.message ?? ''
+        }]);
+
+        variables.set([{
+          id: options.promotionSelectionEnabled,
+          value: Boolean(serviceSelection.showServicePromotions.enabled)
+        }]);
+
+
+      } catch (error) {
+        logs.add({
+          status: "error",
+          description: JSON.stringify(error),
+        });
+      }
+    },
+  },
+});
