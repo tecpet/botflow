e5e01621d60eb3bc2bd2be958225c1c600d0169a
Pablo Zandoná <pablozandona@gmail.com>
2025-08-29 18:00:39 -0300

    feat: add action on continueChat

--- packages/forge/blocks/tecpet-ai/src/actions/continueChat.ts
+++ packages/forge/blocks/tecpet-ai/src/actions/continueChat.ts
@@ -23,9 +23,17 @@ export const continueChat = createAction({
       label: "Salvar resposta em",
       inputType: "variableDropdown",
     }),
+    saveActionInVariableId: option.string.layout({
+      label: "Salvar ação em",
+      inputType: "variableDropdown",
+    }),
   }),
-  getSetVariableIds: ({saveResponseInVariableId}) =>
-    saveResponseInVariableId ? [saveResponseInVariableId] : [],
+  getSetVariableIds: ({saveResponseInVariableId, saveActionInVariableId}) => {
+    const variables = [];
+    if (saveResponseInVariableId) variables.push(saveResponseInVariableId);
+    if (saveActionInVariableId) variables.push(saveActionInVariableId);
+    return variables;
+  },
   run: {
     server: async ({credentials, options, variables, logs}) => {
       try {
@@ -42,10 +50,11 @@ export const continueChat = createAction({
           })
           .json<any>();
 
-        console.log(response)
-
         if (options.saveResponseInVariableId) {
           variables.set([{id: options.saveResponseInVariableId, value: response.response}]);
+        }
+        if (options.saveActionInVariableId) {
+          variables.set([{id: options.saveActionInVariableId, value: response?.action || 'NONE'}]);
         }
       } catch (error) {
         if (error instanceof HTTPError)
