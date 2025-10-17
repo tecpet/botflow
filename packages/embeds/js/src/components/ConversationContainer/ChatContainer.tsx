3b9bacb2092894db98ac13254e33437e0f3c32d5
Pablo Zandoná <pablozandona@gmail.com>
2025-10-17 16:22:10 -0300

    fix: 🐛 (bot) Fix crash when continue message richText is empty Baptiste Arnaud 06/10/25, 09:44

--- packages/embeds/js/src/components/ConversationContainer/ChatContainer.tsx
+++ packages/embeds/js/src/components/ConversationContainer/ChatContainer.tsx
@@ -685,8 +685,8 @@ const sanitizeMessages = (messages: ContinueChatResponse["messages"]) => {
       message.type !== "text" ||
       message.content.type !== "richText" ||
       message.content.richText.length > 1 ||
-      (message.content.richText.length > 1 && message.content.richText[0].type !== "variable") ||
-      (message.content.richText.length > 1 && message.content.richText[0].children && message.content.richText[0].children.length > 0)
+      message.content.richText[0]?.type !== "variable" ||
+      (message.content.richText[0]?.children?.length ?? 0) > 0
     );
   });
 };
